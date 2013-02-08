e6fcfadc0da6b5c38a2b5617758711429941c478
Ao Xu <xuaoxiqi@gmail.com>
2013-02-08 18:01:12 +0800

    Tube LBM bug fix

--- 41.Lattice Boltzmann Method/tube_bodyforce.f90
+++ 41.Lattice Boltzmann Method/tube_bodyforce.f90
@@ -19,14 +19,16 @@
 
         program main
         implicit none
-        integer, parameter :: N=41,M=21
+        integer, parameter :: N=17,M=17
         integer :: i, j, itc, itc_max, k
-        real(8) :: cs2, dx, dy, dt, tau
+        integer :: mid_x, mid_y
+        real(8) :: c, cs2, dx, dy, tau, nu
         real(8) :: g
         real(8) :: eps, error
         real(8) :: X(N), Y(M), u(N,M), v(N,M), up(N,M), vp(N,M), rho(N,M), p(N,M), psi(N,M)
         real(8) :: omega(0:8), f(0:8,N,M), un(0:8)
         real(8) :: ex(0:8), ey(0:8)
+        character (len=100):: filename
         data ex/0.0d0,1.0d0,0.0d0, -1.0d0, 0.0d0, 1.0d0, -1.0d0, -1.0d0, 1.0d0/
         data ey/0.0d0,0.0d0,1.0d0, 0.0d0, -1.0d0, 1.0d0, 1.0d0, -1.0d0, -1.0d0/
 
@@ -38,22 +40,30 @@
 !!!              7   4   8
 
 !!! input initial data
-        cs2 = 1.0d0/3.0d0
-        dx = 2.0d0/float(N-1)
+        dx = 1.0d0/float(N-1)
         dy = 1.0d0/float(M-1)
-        dt = dx
-        tau = 1.0d0
-        g = 1e-4
+
+        tau = 10.0d0
+        nu = 0.5d0
+        c = 3.0*nu/dx/(tau-0.5d0)
+        cs2 = 1.0d0/3.0d0*c*c
+
+        g = 1e-3
+
+        ex = c*ex
+        ey = c*ey
+        nu = c*dx*(tau-0.5d0)/3.0d0
         itc = 0
-        itc_max = 1e5
-        eps = 1e-5
+        itc_max = 3000
+        eps = 1e-6
         k = 0
         error = 100.0d0
 
 !!! set up initial flow field
         call initial(N,M,dx,dy,X,Y,u,v,rho,psi,cs2,omega,ex,ey,un,f)
 
-        do while((error.GT.eps).AND.(itc.LT.itc_max))
+        !do while((error.GT.eps).AND.(itc.LT.itc_max))
+        do while(itc.Lt.itc_max)
 
 !!! streaming step
             call streaming(N,M,f)
@@ -68,11 +78,10 @@
             call check(N,M,u,v,up,vp,itc,error)
 
 !!! output preliminary results
-            if(MOD(itc,10000).EQ.0) then
+            if(MOD(itc,1000).EQ.0) then
                 call calp(N,M,cs2,rho,p)
-                call calpsi(N,M,dx,dy,up,vp,psi)
                 k = k+1
-                call output(N,M,X,Y,up,vp,psi,p,k)
+                call output(N,M,X,Y,up,vp,p,k)
             endif
 
         enddo
@@ -81,17 +90,46 @@
         call calp(N,M,cs2,rho,p)
 
 !!! compute streamfunction
-        call calpsi(N,M,dx,dy,up,vp,psi)
+       ! call calpsi(N,M,dx,dy,up,vp,psi)
 
 !!! output data file
         k = k+1
-        call output(N,M,X,Y,up,vp,psi,p,k)
+        call output(N,M,X,Y,up,vp,p,k)
+
+        write(filename,*) int(tau)
+        filename = adjustl(filename)
+
+        open(unit=02,file='u-y_tau'//trim(filename)//'.dat',status='unknown')
+        write(02,101)
+        write(02,202)
+        write(02,203) M
+
+        mid_x = (N-1)/2+1
+        mid_y = (M-1)/2+1
+
+        do j=1,M
+                write(02,100)  up(mid_x,j), Y(j)
+        enddo
+
+        write(*,*)
+        write(*,*) 'tau=',tau
+        write(*,*) 'c=',c
+        write(*,*) 'u(mid_x,mid_y)', up(mid_x,mid_y)
+        write(*,*)
+
+100     format(2x,10(e12.6,'      '))
+101     format('Title="Lid Driven Cavity Flow"')
+202     format('Variables=U,Y')
+203     format('zone',1x,'i=',1x,i5,2x,'f=point')
+
+        close(02)
 
         write(*,*)
         write(*,*) '************************************************************'
         write(*,*) 'This program sloves Tube Flow problem using Lattice Boltzmann Method'
         write(*,*) 'Driven by Body Force'
         write(*,*) 'N =',N,',       M =',M
+        write(*,*) 'nu=',nu
         write(*,*) 'eps =',eps
         write(*,*) 'itc =',itc
         write(*,*) '************************************************************'
@@ -151,6 +189,8 @@
         integer :: i, j, N, M
         real(8) :: f(0:8,N,M)
 
+
+
         do i=1,N
             do j=1,M
                 f(0,i,j) = f(0,i,j)
@@ -197,8 +237,44 @@
             enddo
         enddo
 
+        !Periodic boundary conditon
+        do j=1,M
+            f(1,1,j) = f(1,N,j)
+            f(5,1,j) = f(5,N,j)
+            f(8,1,j) = f(8,N,j)
+
+            f(3,N,j) = f(3,1,j)
+            f(6,N,j) = f(6,1,j)
+            f(7,N,j) = f(7,1,j)
+        enddo
+
         return
         end subroutine streaming
+
+
+!!! boundary condition
+        subroutine bounceback(N,M,f)
+        implicit none
+        integer :: N, M, i, j
+        real(8) :: f(0:8,N,M)
+
+        do i=1,N
+
+            !Bottom side
+            f(2,i,1) = f(4,i,1)
+            f(5,i,1) = f(7,i,1)
+            f(6,i,1) = f(8,i,1)
+
+            !Up side
+            f(4,i,M) = f(2,i,M)
+            f(7,i,M) = f(5,i,M)
+            f(8,i,M) = f(6,i,M)
+
+        enddo
+
+
+        return
+        end subroutine bounceback
 
 !!! collision step
         subroutine collision(N,M,u,v,ex,ey,rho,f,omega,cs2,tau,g)
@@ -238,8 +314,8 @@
         f(8,1,1) = feq(8,1,1)
 
         !Left up corner
-        f(7,1,M) = feq(5,1,M)
-        f(8,1,M) = feq(7,1,M)
+        f(5,1,M) = feq(5,1,M)
+        f(7,1,M) = feq(7,1,M)
 
         !Right bottom corner
         f(5,N,1) = feq(5,N,1)
@@ -252,40 +328,6 @@
         return
         end subroutine collision
 
-!!! boundary condition
-        subroutine bounceback(N,M,f)
-        implicit none
-        integer :: N, M, i, j
-        real(8) :: f(0:8,N,M)
-
-        do i=2,N-1
-
-            !Bottom side
-            f(2,i,1) = f(4,i,1)
-            f(5,i,1) = f(7,i,1)
-            f(6,i,1) = f(8,i,1)
-
-            !Up side
-            f(4,i,M) = f(2,i,M)
-            f(7,i,M) = f(5,i,M)
-            f(8,i,M) = f(6,i,M)
-
-        enddo
-
-        !Periodic boundary conditon
-        do j=1,M
-            f(1,1,j) = f(1,N,j)
-            f(5,1,j) = f(5,N,j)
-            f(8,1,j) = f(8,N,j)
-
-            f(3,N,j) = f(3,1,j)
-            f(6,N,j) = f(6,1,j)
-            f(7,N,j) = f(7,1,j)
-        enddo
-
-
-        return
-        end subroutine bounceback
 
 !!! check convergence
         subroutine check(N,M,u,v,up,vp,itc,error)
@@ -306,7 +348,7 @@
             do i=1,N
                 do j=1,M
                         error  = error+SQRT((u(i,j)-up(i,j))*(u(i,j)-up(i,j))+(v(i,j)-vp(i,j))*(v(i,j)-vp(i,j))) &
-                                        /SQRT((u(i,j)+0.00001)*(u(i,j)+0.00001)+(v(i,j)+0.00001)*(v(i,j)+0.00001))
+                                        /SQRT((u(i,j)+0.000001)*(u(i,j)+0.000001)+(v(i,j)+0.00001)*(v(i,j)+0.000001))
                 enddo
             enddo
         endif
@@ -314,13 +356,13 @@
         up = u
         vp = v
 
-        if(MOD(itc,200).EQ.0) write(*,*) itc,' ',error
+        if(MOD(itc,50).EQ.0) write(*,*) itc,' ',error
 
-!!!        open(unit=01,file='error.dat',status='unknown',position='append')
-!!!        if (MOD(itc,2000).EQ.0) then
-!!!            write(01,*) itc,' ',error
-!!!        endif
-!!!        close(01)
+!        open(unit=01,file='error.dat',status='unknown',position='append')
+!        if (MOD(itc,2000).EQ.0) then
+!            write(01,*) itc,' ',error
+!        endif
+!        close(01)
 
         return
         end subroutine check
@@ -333,7 +375,7 @@
         real(8) :: rho(N,M), p(N,M)
 
         do i=1,N
-            do j=1,M-1
+            do j=1,M
                 p(i,j) = rho(i,j)*cs2
             enddo
         enddo
@@ -341,69 +383,30 @@
         return
         end subroutine calp
 
-!!! compute Streamfunction
-        subroutine calpsi(N,M,dx,dy,u,v,psi)
-        implicit none
-        integer :: N, M, i, j
-        real(8) :: dx, dy
-        real(8) :: u(N,M), v(N,M), psi(N,M)
-
-!        do j=1,M
-!            psi(1,j) = 0.0d0
-!            psi(N,j) = 0.0d0
-!        enddo
-!        do i=1,N
-!            psi(i,1) = 0.0d0
-!            psi(i,M) = 0.0d0
-!        enddo
-
-        do i=3,N-2
-            do j=2,M-3
-            psi(i,j+1) = u(i,j)*2.0d0*dy+psi(i,j-1)
-            !psi(i+1,j) = -v(i-1,j)*2.0d0*dx+psi(i-1,j) ! Alternative and equivalent psi formulae
-            enddo
-        enddo
-
-        do j=2,M-1
-            psi(2,j) = 0.25d0*psi(3,j)
-            psi(N-1,j) = 0.25d0*psi(N-2,j)
-        enddo
-        do i=2,N-1
-            psi(i,2) = 0.25d0*psi(i,3)
-            psi(i,M-1) = 0.25d0*psi(i,M-2)
-        enddo
-
-        return
-        end subroutine calpsi
-
 !!! output data file
-        subroutine output(N,M,X,Y,up,vp,psi,p,k)
+        subroutine output(N,M,X,Y,up,vp,p,k)
         implicit none
         integer :: N, M, i, j, k
-        real(8) :: X(N), Y(M), up(N,M), vp(N,M), psi(N,M), p(N,M)
+        real(8) :: X(N), Y(M), up(N,M), vp(N,M), p(N,M)
+        character (len=100):: filename
 
-        character*16 filename
+        write(filename,*) k
+        filename = adjustl(filename)
+        open(unit=02,file='output_'//trim(filename)//'.dat',status='unknown')
 
-        filename='0000tube.dat'
-        filename(1:1) = CHAR(ICHAR('0')+MOD(k/1000,10))
-        filename(2:2) = CHAR(ICHAR('0')+MOD(k/100,10))
-        filename(3:3) = CHAR(ICHAR('0')+MOD(k/10,10))
-        filename(4:4) = CHAR(ICHAR('0')+MOD(k,10))
-
-        open(unit=02,file=filename,status='unknown')
         write(02,101)
         write(02,102)
         write(02,103) N, M
 
         do j=1,M
             do i=1,N
-                write(02,100) X(i), Y(j), up(i,j), vp(i,j), psi(i,j), p(i,j)
+                write(02,100) X(i), Y(j), up(i,j), vp(i,j)!, p(i,j)
             enddo
         enddo
 
 100     format(2x,10(e12.6,'      '))
 101     format('Title="Tube Flow"')
-102     format('Variables=x,y,u,v,psi,p')
+102     format('Variables=x,y,u,v')
 103     format('zone',1x,'i=',1x,i5,2x,'j=',1x,i5,1x,'f=point')
 
         close(02)
